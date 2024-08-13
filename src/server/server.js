0a60513f162561c0d7c5f211e51ec421adc1965a
alejandro isea <alejandroisea32@gmail.com>
2024-08-13 11:44:43 -0300

    realizamos cambios en el servidor

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
-const express = require('express')  
-const path = require('path')
+import express from 'express'  
+import path from 'path'
 
 const starServer = (options) =>{
     const {port, public_path = 'public'} = options
